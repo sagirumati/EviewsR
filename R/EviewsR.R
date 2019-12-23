02d9a38d6de892e2e79835496c05ca2b50e25360
sagirumati <smatiweb@gmail.com>
2019-12-23 18:00:02 +0200

    change the path from absolute to relative  '%s/%s' to '%s'.
    Signed-off-by: sagirumati <smatiweb@gmail.com>

--- R/EviewsR.R
+++ R/EviewsR.R
@@ -12,7 +12,7 @@ eng_Eviews <- function(options) {
   # if eval != FALSE compile/run the code, preserving output
 
   if (options$eval) {
-    out <- shell(sprintf('%s/%s',paste(path),paste(f, options$engine.opts)))
+    out <- shell(sprintf('%s',paste(f, options$engine.opts)))
   }
 
   # spit back stuff to the user
