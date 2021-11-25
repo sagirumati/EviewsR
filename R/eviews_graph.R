b1ce02ed6c051ac1dd053d7b48eadde5c7667f5b
sagirumati <smatiweb@gmail.com>
2021-11-25 09:43:39 +0100

    appending dpi=300 and finished documentation

--- R/eviews_graph.R
+++ R/eviews_graph.R
@@ -1,20 +1,23 @@
-#' Create an `EViews` graph in R Markdown
+#' Create an `EViews` graph in R and R Markdown
 #'
-#' Use this function to create an `EViews` graph in R Markdown
+#' Use this function to create an `EViews` graph in R and R Markdown
 #'
-#' @usage eviews_wfcreate(wf_name="",page_name="",frequency="",start_date="",end_date="",path="",save=T)
-#' @param wf_name Object or a character string representing the name of a workfile to be created
-#'
-#' @param page_name Object or a character string representing the name of a workfile page to be created
-#'
+#' @usage eviews_graph(series="",wf="",page="",mode="overwrite",graph_command="line",options="m",frequency="7",start_date="",save=FALSE,save_options=c("t=png","color"),save_path="",graph_procs=c('textdefault font("Times",20,-b,-i,-u,-s)','align(2,1,1)'),datelabel="",merge_graphs=FALSE)
+#' @param series A vector of series names contained in an `EViews` workfile, or an R dataframe.
+#' @param wf Object or a character string representing the name of an `EViews` workfile.
+#' @param save_options A vector of options to be passed to `EViews` \code{save} command. It can values like \code{"t=png"},\code{-color} and so on.
+#' @param page Object or a character string representing the name of an `EViews` workfile page.
+#' @param mode Set `mode="overwrite"` to overwrite existing `EViews` graph objects that match the new `EViews` graph object to be created on the workfile. Set `mode=""` to avoid overwriting exising `EViews` graph object.
+#' @param graph_command Object or a character string of any of the acceptable `EViews` graphical commands, such as \code{line}, \code{bar}, \code{pie}.
+#' @param options Object or a character string of any of the acceptable `EViews` graphical options, such as \code{""}, \code{m}, \code{s}.
 #' @param frequency Object or a character string representing the frequency of a workfile page to be created. Only letters accepted by EViews are allowed. For example \code{u} for undated, \code{a} for annual, \code{m} for monthly and so on.
-#'
+#' @param save Logical, whether to save the `EViews` graphs on disk.
 #' @param start_date Object or a character string representing the \code{start date}. It should be left blank for undated (when the \code{frequency} is \code{u}).
-#'
+#' @param graph_procs A vector containing `EViews` graph \code{procs} such as \code{datelabel}, \code{align}
 #' @param end_date Object or a character string representing the \code{end date}. It should be left blank for undated (when the \code{frequency} is \code{u}).
-#'
-#' @param path Object or a character string representing the path to the folder for the  workfile to be saved. The current working directory is the default `path`. Specify the `path` only if you want the workfile to live in different path from the current working directory.
-#'
+#' @param datelabel A vector containing `EViews` axis label formats such as \code{format("YY")}. Using \code{datelabel} in \code{graph_procs} overwrites this argument.
+#' @param save_path Object or a character string representing the path to the folder to save the `EViews` graphs. The current working directory is the default `save_path`. Specify the `save_path` only if you want the `EViews` graphs to live in different path from the current working directory.
+#' @param merge_graphs Logical, whether to merge two or more graphs on one page. Setting \code{merge_graphs=FALSE} produces `EViews` graph for each series separately.
 #' @return An EViews workfile
 #'
 #' @examples library(EviewsR)
@@ -29,23 +32,25 @@ eviews_graph=function(series="",wf="",page="",mode="overwrite",graph_command="li
     if(frequency!="" & start_date!=""){
     stopifnot("The 'series' object must be a dataframe"=is.data.frame(series))
     wf=basename(tempfile("EViewsR"))
+    wf1=wf
+
     csvFile=paste0(wf,".csv")
         write.csv(series,csvFile,row.names = F)
         eviews_import(source_description = csvFile,frequency = frequency,start_date = start_date)
 
   series = names(series);wf=wf
-        on.exit(unlink(c(csvFile,paste0(wf,".wf1"))))
-          }
+}
 
   if(is.data.frame(series)) series1=names(series) else series1=series
 
   save_options1=c("t=bmp","t=gif", "t=jpeg", "t=png")
-  if(intersect(save_options,save_options1) %in% save_options1) save_options=append(save_options,"dpi=300")
+
+  # Append "dpi=300" if "dpi" is not defined in "save_options"
+  if(intersect(save_options,save_options1) %in% save_options1 & sum(grepl("dpi",save_options, ignore.case = T))==0) save_options=append(save_options,"dpi=300")
 
   #stopifnot("EViewsR works on Windows only"=Sys.info()["sysname"]=="Windows")
-  # stopifnot("Please enter at least one series name"=!is.null(series),series!="",series!=" ",series %in% letters | series %in% LETTERS)
-  #stopifnot("Please enter at least one series name"=series %in% letters | series %in% LETTERS,!is.null(series))
-  fileName=tempfile("EVIEWS", ".", ".prg")
+
+    fileName=tempfile("EVIEWS", ".", ".prg")
   EviewsRGroup=basename(tempfile("EviewsRGroup"))
 
 
@@ -66,12 +71,10 @@ datelabel=paste('{%y}.datelabel',datelabel)
   graph_procs=paste0("{%y}.",graph_procs)
   graph_procs=append(c('%allEviewsGraphs=@wlookup("*","graph")\n','for %y {%allEviewsGraphs}\n')
 ,c(datelabel,graph_procs,'next'))
-  # graphProcs=wf=paste0(graphProcs)
 
   EviewsRGroup=paste0('%EviewsRGroup=',shQuote(EviewsRGroup))
    wf=paste0('%wf=',shQuote(wf))
     page=paste0("%page=",shQuote(page))
-    # series=paste0("%z=@wlookup(",shQuote(paste(series,collapse = " ")),',"series")')
     series=paste(series,collapse = " ")
     series=paste0("%series=",shQuote(series))
     graph_command=paste0("%graph_command=",shQuote(graph_command))
@@ -98,7 +101,6 @@ endif
 
 if %mode<>"" then
 %mode="mode="+%mode+","
-string mode="mode="+%mode+","
 endif
 
 
@@ -138,7 +140,9 @@ next)'
 
 save_code=r'(for !k=1 to {!n}
 {%x{!k}}_graph_EviewsR.save{%save_options} {%save_path}{%x{!k}}_graph_EviewsR
-next)'
+next
+delete {%EviewsrGroup}
+exit)'
 }
 
     if (merge_graphs==TRUE){
@@ -155,8 +159,8 @@ endif
 
 if %mode<>"" then
 %mode="mode="+%mode+","
-string mode="mode="+%mode+","
 endif
+
 
 %z=@wlookup(%series,"series")
 %graph_command=@wreplace(%graph_command,"* ","*")
@@ -164,6 +168,12 @@ endif
 %file_name=@wreplace(%file_name,"* ","*")
 %save_path=@wreplace(%save_path,"* ","*")
 %save_path=@wreplace(%save_path,"/","\")
+
+
+if %save_path<>"" then
+%save_path=%save_path+"\"
+endif
+
 %save_options=@wreplace(%save_options,"* ","*")
 
 if %save_options<>"" then
@@ -181,13 +191,17 @@ group {%EviewsRGroup} {%z}
 %seriesNames=@replace(%z," ","")
 freeze({%mode}{%seriesNames}_graph_EviewsR) {%EviewsRGroup}.{%graph_command}{%options})'
 
-save_code=r'({%seriesNames}_graph_EviewsR.save{%save_options} {%save_path}{%seriesNames}_graph_EviewsR)'
+save_code=r'({%seriesNames}_graph_EviewsR.save{%save_options} {%save_path}{%seriesNames}_graph_EviewsR
+delete {%EviewsrGroup}
+exit)'
 }
 
 writeLines(c(eviews_path(),EviewsRGroup,wf,page,series,graph_command,options,mode,file_name,save_path,save_options,eviews_code,graph_procs,save_code), fileName)
 
 system_exec()
 on.exit(unlink_eviews(),add = TRUE)
+on.exit(unlink(csvFile,force = T),add = T)
+on.exit(unlink(paste0(wf1,".wf1"),force = T),add = T)
 
 if(merge_graphs==TRUE){
   series1=paste(series1,collapse = "")
@@ -225,7 +239,6 @@ if (save==TRUE){
 
 }
 
-
-# NEXT IS TO REMOVE DATA WHEN ATTACHED TO THE NAME OF MERGED GRAPHS
+# DELETE CSV and WORKFILES
 
 # eviews_graph(wf="",page = "page",series="x y",mode = "overwrite",options = "m")
