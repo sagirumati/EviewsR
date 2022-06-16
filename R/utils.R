4ea42f3800e51972f9bea5a3f8bba5fd0e620fed
sagirumati <smatiweb@gmail.com>
2022-06-16 14:40:33 +0100

    update 2022-06-16 14:40:33

--- R/utils.R
+++ R/utils.R
@@ -50,7 +50,8 @@ if(exists('table_name.csv',envir = parent.frame()))  table_name.csv=eval(express
   knitr::knit_engines$set(eviews=eng_eviews)
   # set_eviews_path()
   if(!exists("eviews") || !is.environment(eviews)) eviews<<-new.env()
-  }
+  opts_current$set(opts_chunk$get())
+    }
 
 
 
