1ce135613f5ae67345b02a532831e8576c867407
sagirumati <smatiweb@gmail.com>
2022-06-24 19:40:07 +0100

    update 2022-06-24 19:40:07

--- inst/EVIEWS/hexcode.R
+++ inst/EVIEWS/hexcode.R
@@ -16,14 +16,15 @@ next
 group gr z y x
 freeze(zyx) gr.line
 zyx.template eviews10
-zyx.options size(3,3)
+zyx.options size(3,3) gridwidth(.5)
 zyx.save(d=300,t=png) zyx
 '
 exec_commands(commands)
  hexSticker::sticker("eviews.pdf",package = "EviewsR",s_width =2.5,s_height = 2.3,s_x = 0.27,s_y = 1,p_color = "#01B5F2",spotlight=T,p_y = 1.8,h_color = "#01B5F2",white_around_sticker=T)
 
 
- # good one#hexSticker::sticker("zyx.png",package = "EviewsR1",s_width =1.8,s_height = 1.8,s_x = 1.27,s_y = 1,p_color = "#01B5F2",spotlight=T,p_y = 1.8,h_color = "#01B5F2",white_around_sticker=T)
-  hexSticker::sticker("zyx.png",asp = 1.1,package = "EviewsR1",s_width =1.9,s_height = 1.8,s_x = 1.26098,s_y = 0.94,p_color = "#01B5F2",spotlight=T,p_y = 1.8,h_color = "#01B5F2",white_around_sticker=T,url="www.smati.com.ng")
+ hexSticker::sticker("zyx.png",package = "EviewsR",s_width =1.8,s_height = 1.8,s_x = 1.27,s_y = 0.94,p_color = "#E391BC",spotlight=T,p_y = 1.8,h_color = "#01B5F2",white_around_sticker=T)
+  # hexSticker::sticker("zyx.png",asp = 1.05,package = "EviewsR",s_width =1.9,s_height = 1.8,s_x = 1.26098,s_y = 0.94,p_color = "#01B5F2",spotlight=T,p_y = 1.8,h_color = "#01B5F2",white_around_sticker=T)
 
  shell.exec("Eviewsr1.png")
+ # shell.exec("Eviewsr.png")
