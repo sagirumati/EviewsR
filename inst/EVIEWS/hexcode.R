99e4c02f4e3ae30827c4922d87e89a88479abd91
sagirumati <smatiweb@gmail.com>
2022-06-24 15:40:04 +0100

    update 2022-06-24 15:40:04

--- inst/EVIEWS/hexcode.R
+++ inst/EVIEWS/hexcode.R
@@ -16,10 +16,14 @@ next
 group gr z y x
 freeze(zyx) gr.line
 zyx.template eviews10
+zyx.options size(3,3)
 zyx.save(d=300,t=png) zyx
 '
 exec_commands(commands)
  hexSticker::sticker("eviews.pdf",package = "EviewsR",s_width =2.5,s_height = 2.3,s_x = 0.27,s_y = 1,p_color = "#01B5F2",spotlight=T,p_y = 1.8,h_color = "#01B5F2",white_around_sticker=T)
 
 
- hexSticker::sticker("zyx.png",package = "EviewsR1",s_width =2.5,s_height = 2.3,s_x = 0.27,s_y = 1,p_color = "#01B5F2",spotlight=T,p_y = 1.8,h_color = "#01B5F2",white_around_sticker=T)
+ # good one#hexSticker::sticker("zyx.png",package = "EviewsR1",s_width =1.8,s_height = 1.8,s_x = 1.27,s_y = 1,p_color = "#01B5F2",spotlight=T,p_y = 1.8,h_color = "#01B5F2",white_around_sticker=T)
+  hexSticker::sticker("zyx.png",asp = 1.1,package = "EviewsR1",s_width =1.8,s_height = 1.8,s_x = 1.2608,s_y = 0.94,p_color = "#01B5F2",spotlight=T,p_y = 1.8,h_color = "#01B5F2",white_around_sticker=T)
+
+ shell.exec("Eviewsr1.png")
