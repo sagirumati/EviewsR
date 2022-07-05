29f9b532ea475747c6076cd9fda536991990524a
sagirumati <smatiweb@gmail.com>
2022-07-05 11:40:02 +0100

    update 2022-07-05 11:40:02

--- R/eng_Eviews.R
+++ R/eng_Eviews.R
@@ -60,6 +60,87 @@ eng_eviews <- function(options) {
      options$page=TRUE
    } else pageList='%pagelist1=""'
 
+   graphicsDefault=r'(
+   %pagelist=@pagelist
+
+   if %pagelist1<>"" then
+   %pagelist=%pagelist1
+   endif
+
+   for %page {%pagelist}
+   pageselect {%page}
+
+
+   if %figKeep1="first" then
+   %figKeep=@wlookup("*","graph")
+   %figKeep=@wleft(%figKeep,1)
+   endif
+
+   if %figKeep1="last" then
+   %figKeep=@wlookup("*","graph")
+   %figKeep=@wright(%figKeep,1)
+   endif
+
+   if %figKeep1="all" then
+   %figKeep=@wlookup("*","graph")
+   endif
+
+   if %figKeep1="none" then
+   %figKeep=""
+   endif
+
+   if %figKeep1="" then
+   %figKeep=@wlookup("*","graph")
+   endif
+
+
+   if @wcount(%figKeep)>0 then
+   for %y {%figKeep}
+   {%y}.axis(l) font(Calibri,14,-b,-i,-u,-s)
+   {%y}.axis(r) font(Calibri,14,-b,-i,-u,-s)
+   {%y}.axis(b) font(Calibri,14,-b,-i,-u,-s)
+   {%y}.axis(t) font(Calibri,14,-b,-i,-u,-s)
+   {%y}.legend columns(5) inbox position(BOTCENTER) font(Calibri,12,-b,-i,-u,-s)
+   {%y}.options antialias(on)
+   {%y}.options size(6,3)
+   {%y}.options -background frameaxes(all) framewidth(0.5)
+   {%y}.setelem(1) linecolor(@rgb(57,106,177)) linewidth(1.5)
+   {%y}.setelem(2) linecolor(@rgb(218,124,48)) linewidth(1.5)
+   {%y}.setelem(3) linecolor(@rgb(62,150,81)) linewidth(1.5)
+   {%y}.setelem(4) linecolor(@rgb(204,37,41)) linewidth(1.5)
+   {%y}.setelem(5) linecolor(@rgb(83,81,84)) linewidth(1.5)
+   {%y}.setelem(6) linecolor(@rgb(107,76,154)) linewidth(1.5)
+   {%y}.setelem(7) linecolor(@rgb(146,36,40)) linewidth(1.5)
+   {%y}.setelem(8) linecolor(@rgb(148,139,61)) linewidth(1.5)
+   {%y}.setelem(9) linecolor(@rgb(255,0,255)) linewidth(1.5)
+   {%y}.setelem(10) linewidth(1.5)
+   {%y}.setelem(11) linecolor(@rgb(192,192,192)) linewidth(1.5)
+   {%y}.setelem(12) linecolor(@rgb(0,255,255)) linewidth(1.5)
+   {%y}.setelem(13) linecolor(@rgb(255,255,0)) linewidth(1.5)
+   {%y}.setelem(14) linecolor(@rgb(0,0,255)) linewidth(1.5)
+   {%y}.setelem(15) linecolor(@rgb(255,0,0)) linewidth(1.5)
+   {%y}.setelem(16) linecolor(@rgb(0,127,0)) linewidth(1.5)
+   {%y}.setelem(17) linecolor(@rgb(0,0,0)) linewidth(1.5)
+   {%y}.setelem(18) linecolor(@rgb(0,127,127)) linewidth(1.5)
+   {%y}.setelem(19) linecolor(@rgb(127,0,127)) linewidth(1.5)
+   {%y}.setelem(20) linecolor(@rgb(127,127,0)) linewidth(1.5)
+   {%y}.setelem(21) linecolor(@rgb(0,0,127)) linewidth(1.5)
+   {%y}.setelem(22) linecolor(@rgb(255,0,255)) linewidth(1.5)
+   {%y}.setelem(23) linecolor(@rgb(127,127,127)) linewidth(1.5)
+   {%y}.setelem(24) linecolor(@rgb(192,192,192)) linewidth(1.5)
+   {%y}.setelem(25) linecolor(@rgb(0,255,255)) linewidth(1.5)
+   {%y}.setelem(26) linecolor(@rgb(255,255,0)) linewidth(1.5)
+   {%y}.setelem(27) linecolor(@rgb(0,0,255)) linewidth(1.5)
+   {%y}.setelem(28) linecolor(@rgb(255,0,0)) linewidth(1.5)
+   {%y}.setelem(29) linecolor(@rgb(0,127,0)) linewidth(1.5)
+   {%y}.setelem(30) linecolor(@rgb(0,0,0)) linewidth(1.5)
+   {%y}.setfont legend(Calibri,12,-b,-i,-u,-s) text(Calibri,14,-b,-i,-u,-s) obs(Calibri,14,-b,-i,-u,-s) axis(Calibri,14,-b,-i,-u,-s)
+   {%y}.setfont obs(Calibri,14,-b,-i,-u,-s)
+   {%y}.textdefault font(Calibri,14,-b,-i,-u,-s)
+   next
+   endif
+   next
+   )'
 
    if (!is.null(options$graph_procs)){
      graph_procs=options$graph_procs
@@ -434,7 +515,7 @@ eng_eviews <- function(options) {
    #   {%series}
    #   )'
 
-   eviewsCode=paste0(c(eviews_path(),pageList,figKeep,eviewsrText,chunkName1,save_path,options$code,save_options,graphicsDefault(),graph_procs,figSave,saveCode), collapse = "\n") %>%
+   eviewsCode=paste0(c(eviews_path(),pageList,figKeep,eviewsrText,chunkName1,save_path,options$code,save_options,graphicsDefault,graph_procs,figSave,saveCode), collapse = "\n") %>%
      strsplit(split="\n") %>% unlist()
 
    # writeLines(eviewsCode,fileName)
