d0da45980c83d1e0eed13f4b381d1c3c23c0fc0e
sagirumati <smatiweb@gmail.com>
2022-07-21 10:40:04 +0100

    update 2022-07-21 10:40:04

--- R/import_series.R
+++ R/import_series.R
@@ -76,8 +76,11 @@ import_series=function(wf="",page="*",series="*"){
   %seriesPath=""
   for %page {%pagelist}
   pageselect {%page}
-  pagesave {%page}-{%chunkName}{%eviewsrText}.csv @drop date
+  %series=@wlookup(%series,"series")
+  if @wcount(%series)>0 then
+  pagesave {%page}-{%chunkName}{%eviewsrText}.csv @keep {%series} @drop date
   %seriesPath=%seriesPath+" "+%page+"-"+%chunkName+%eviewsrText
+  endif
   next
 
   text {%eviewsrText}_series
