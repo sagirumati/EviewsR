6222f59666b5891005f7e4358f2d68b6d0c2d692
sagirumati <smatiweb@gmail.com>
2022-08-03 18:16:40 +0100

    About sanitize comments and functions

--- R/import_graph.R
+++ R/import_graph.R
@@ -251,7 +251,6 @@ on.exit(unlink(paste0(eviewsrText1,'-graph.txt')),add = TRUE)
 #
 # for (i in graph1) eviews_graphics=append(eviews_graphics,list.files(pattern=paste0("^",chunkName1,i,"\\.",extension,"$"),path=save_path1,ignore.case = T))
 #
-# # b=list.files(paste0("^",a[1],".png","$"),path = ".")
 
 
 if(file.exists(paste0(eviewsrText1,"-graph.txt"))){
