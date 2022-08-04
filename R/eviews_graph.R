b667bee70ad4ba16840823bcc00d9a2e94cb4f0e
sagirumati <smatiweb@gmail.com>
2022-08-04 08:44:09 +0100

    update 2022-08-04 08:44:01

--- R/eviews_graph.R
+++ R/eviews_graph.R
@@ -20,7 +20,9 @@
 #' @examples library(EviewsR)
 #' \dontrun{
 #' demo(exec_commands)
-#' eviews_graph(wf="EviewsR_exec_commands",page = "page",series="x y",mode = "overwrite",graph_options = "m")
+#' eviews_graph(wf="EviewsR_exec_commands",page = "page",series="x y",mode = "overwrite",
+#' graph_options = "m")
+#'
 #'
 #'}
 #' @family important functions
